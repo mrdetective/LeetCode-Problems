8ef64b6c36cbdc55b1f9f8ea20b56319e15fccc3
Harsha Gupta <74111640+mrdetective@users.noreply.github.com>
2023-12-17 00:25:51 +0530

    Time: 22 ms (59.26%) | Memory: 72.1 MB (9.45%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -10,27 +10,27 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2, int c = 0){ 
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2, int c = 0){
+        if(!l1 && !l2 && !c){
+            return NULL;
+        } 
+        ListNode *l3 = new ListNode();
         if(l1 && l2){
-            ListNode *l3 = new ListNode();
             l3->val = (l1->val + l2->val + c) % 10;
             l3->next = addTwoNumbers(l1->next, l2->next, (l1->val + l2->val + c) / 10);
             return l3;
         }
         else if(l1){
-            ListNode *l3 = new ListNode();
             l3->val = (l1->val + c) % 10;
             l3->next = addTwoNumbers(l1->next, l2, (l1->val + c) / 10);
             return l3;
         }   
         else if(l2){
-            ListNode *l3 = new ListNode();
             l3->val = (l2->val + c) % 10;
             l3->next = addTwoNumbers(l1, l2->next, (l2->val + c) / 10);
             return l3;
         }
         else if(c){
-           ListNode *l3 = new ListNode();
            l3->val = c;
            l3->next = NULL;
            return l3;
